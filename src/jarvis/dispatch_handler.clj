f04bb176d2d819a8bf1ba9ae0995d9876f216e36
cshinaver <bshinaver@gmail.com>
2017-02-21 15:11:22 -0500

    added stutter?

--- src/jarvis/dispatch_handler.clj
+++ src/jarvis/dispatch_handler.clj
@@ -1,5 +1,6 @@
 (ns jarvis.dispatch-handler
-  (:require [jarvis.irc :as irc]))
+  (:require [jarvis.irc :as irc]
+            [jarvis.util :as util]))
 
 
 (def command-words #{"buify"})
@@ -20,16 +21,24 @@
   []
   '("Usage:"
     "        (buify url)    Returns buified image"
-    "        (troll-bobbit) Trolls bobbit"))
+    "        (troll-bobbit) Trolls bobbit"
+    "        (stutter? recipient)"))
 
 (defn troll-bobbit
   []
   (irc/send-message "#lug" "bobbit: Do you have a purpose?"))
 
+(defn send-stutter
+  "Did i stutter?"
+  [channel recipient]
+  (let [link (util/upload-img "resources/stutter.jpg")]
+    (if (not= channel irc/username)
+      (irc/send-message channel (str recipient ": " link)))))
+
 (def handle-lisp? (comp some? (partial re-find #"^\([a-zA-Z-?]+ ?(.*)\)")))
 
 (defn handle-lisp
-  [recipient msg]
+  [recipient channel msg]
   (let [m (re-find #"\(([a-zA-Z-?]+) ?(.*)\)" msg)
         call (second m)
         arg (nth m 2 nil)]
@@ -40,6 +49,7 @@
                          (partial irc/send-message recipient)
                          (usage)))
       (= call "troll-bobbit") (troll-bobbit)
+      (= call "stutter?") (send-stutter channel arg)
       :else (doall (map (partial irc/send-message recipient) (usage))))))
 
 (defn dispatch-handler
@@ -47,12 +57,14 @@
   (let [msg (:text s)
         sender (:nick s)
         link (irc/pull-link-from-message msg)
-        recipient (irc/determine-recipient s)]
+        recipient (irc/determine-recipient s)
+        channel (:target s)]
     (cond
-      (handle-lisp? msg) (handle-lisp recipient msg)
+      (handle-lisp? msg) (handle-lisp recipient channel msg)
       (and (not= irc/username sender) (or (irc/message-contains? #"hotbot:" msg) (irc/message-contains? #"@hotbot" msg)))
       (cond
-        (handle-message-link? msg) (handle-message-link recipient msg) 
+        (handle-message-link? msg) (handle-message-link recipient msg)
         (handle-whose-better-than-bobbit? msg) (handle-whose-better-than-bobbit recipient)
         :else (irc/send-message recipient "wut")))))
+
 
